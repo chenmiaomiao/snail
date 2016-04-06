80d0a9f5261ae6ff06b95276f007312e3a7b3221
oldse <oldse@DESKTOP-MRHQCQ1>
2016-04-06 20:29:06 +0800

    1

--- src/database.py
+++ src/database.py
@@ -1,6 +1,10 @@
 import sqlite3
-print "hello"
+
 conn = sqlite3.connect('stock.sqlite3')
 cur = conn.cursor()
 
 cur.execute('INSERT INTO Majorholderinfo (shareholder, quantity, percentage, equitytype, holderid, stockid, uptodate, reportdate, holdersnumber, averageshares) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, )')
+
+conn.commit()
+cur.close()
+conn.close()
--- src/querydatabase.py
+++
@@ -1,6 +0,0 @@
-# fhand = open('database.txt', 'r')
-# for line in fhand:
-#     line_lst = line.split(',')
-#     print type(line_lst)
-#     print line_lst
-# fhand.close()
